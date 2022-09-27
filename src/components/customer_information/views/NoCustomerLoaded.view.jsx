4a3cad3123143987e7e9069e93e41826f12b4078
Thomas Waldorf <zeeroh71@gmail.com>
2022-09-27 17:18:08 -0400

    Removed MUI from NoCustomerLoaded

--- src/components/customer_information/views/NoCustomerLoaded.view.jsx
+++ src/components/customer_information/views/NoCustomerLoaded.view.jsx
@@ -1,21 +1,10 @@
-import React from "react";
-
-import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
-import { Typography } from "@mui/material";
-import { ThemeProvider } from "@mui/material";
-import { lightTheme } from "../../../theme/Theme";
+import "../../../global_style/style.css";
 
 const NoCustomerLoaded = () => {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <div style={{ padding: "8px" }}>
-        <Grid2 container justifyContent="center" alignContent="center">
-          <Typography variant="h5" color="primary">
-            No Customer Loaded
-          </Typography>
-        </Grid2>
-      </div>
-    </ThemeProvider>
+    <div className="containerRow">
+      <p className="centerContent">No Customer Loaded</p>
+    </div>
   );
 };
 
